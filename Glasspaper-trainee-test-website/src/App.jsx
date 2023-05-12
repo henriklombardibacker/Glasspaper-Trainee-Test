07e12ba52d221a042d4c73cb16732f7c40b0c331
henriklombardibacker <117280176+henriklombardibacker@users.noreply.github.com>
2023-05-12 15:16:45 +0200

    Made Vite project & installed tailwind

---
+++ Glasspaper-trainee-test-website/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className="underline">
+          Hello world!
+        </h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default App
